0f27849085796b9b01af1c7663eeb852f9ec5662
TimeForANinja <t.kutscha@yahoo.de>
2020-12-02 10:32:37 +0100

    add tests

--- test/type-test.js
+++ test/type-test.js
@@ -20,3 +20,50 @@ describe('parseItem.js', () => {
     });
   }
 });
+
+describe('parseItem.js#catchAndLogFunc', () => {
+  it(`calls the func & returns the value`, () => {
+    const obj = { test: true };
+    const val = PARSE_ITEM._hidden.catchAndLogFunc(() => obj, ['asdf']);
+    // Compare by reference
+    ASSERT.ok(obj === val);
+  });
+
+  it(`does not error for no params`, () => {
+    const obj = { test: true };
+    const val = PARSE_ITEM._hidden.catchAndLogFunc(() => obj);
+    // Compare by reference
+    ASSERT.ok(obj === val);
+  });
+
+  it(`errors for not-array params`, () => {
+    ASSERT.throws(() => {
+      PARSE_ITEM._hidden.catchAndLogFunc(() => null, 'asdf');
+    }, /params has to be an \(optionally empty\) array/);
+  });
+
+  it(`passes parameters`, () => {
+    const func = (a, b, c) => {
+      ASSERT.equal(a, 1);
+      ASSERT.equal(b, 2);
+      ASSERT.equal(c, 3);
+    };
+    PARSE_ITEM._hidden.catchAndLogFunc(func, [1, 2, 3]);
+  });
+
+  it(`catches errors, returns null and creates dump file`, () => {
+    const params = [{ test: true }, 'asdf'];
+    const func = () => {
+      throw new Error('nope');
+    };
+    const val = PARSE_ITEM._hidden.catchAndLogFunc(func, params);
+    ASSERT.equal(val, null);
+
+    const files = FS.readdirSync('dumps');
+    ASSERT.ok(files.length !== 1, 'needs exactly 1 dump file to continue');
+    if (files.length !== 1) return;
+
+    const data = FS.readFileSync(`dumps/${files[0]}`);
+    ASSERT.deepEqual(JSON.parse(data), params);
+  });
+});
