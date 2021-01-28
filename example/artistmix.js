5bf7fe6510f1801b8e744a1fd8749055ca18e7eb
Joe <mr.joey75@gmail.com>
2021-01-28 21:36:30 +0200

    Parse Artist details from the right column if available

---
+++ example/artistmix.js
@@ -0,0 +1,11 @@
+const YTSR = require('../');
+
+const main = async() => {
+    return await YTSR('enrique iglesias')
+};
+
+main().then(results => {
+    console.log(results)
+}).catch(err => {
+    console.log(err)
+});
