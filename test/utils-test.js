e7a44b9651a3ee91a86d06b402c442926b6e0ac0
TimeForANinja <t.kutscha@yahoo.de>
2021-04-10 22:51:04 +0200

    update Tests

--- test/utils-test.js
+++ test/utils-test.js
@@ -102,7 +102,7 @@ describe('utils.parseText()', () => {
     );
   });
 
-  it('parges from runs', () => {
+  it('parses from runs', () => {
     ASSERT.equal(
       UTILS.parseText({ runs: [{ text: 'a ' }, { text: 'b' }, { text: ' c' }] }),
       'a b c',
@@ -113,6 +113,13 @@ describe('utils.parseText()', () => {
     ASSERT.equal(
       UTILS.parseText({ simpleText: 'simpleText', runs: [{ text: 'a' }] }),
       'simpleText',
+    );
+  });
+
+  it('picks default withput simpletext and runs', () => {
+    ASSERT.equal(
+      UTILS.parseText({ runs: 'runs is no array' }, 'default-value'),
+      'default-value',
     );
   });
 });
